92a125a9eff7569895f453812758100dc2cc62e6
alexanderStefanovS <a.stefanov_grad@abv.bg>
2021-04-27 18:10:13 +0300

    js 2 exercise

---
+++ exercise10-js-2/ajax.js
@@ -0,0 +1,33 @@
+
+// console.log('start');
+
+// setTimeout(() => {
+
+//     console.log('5 seconds later...');
+
+//     setTimeout(() => {
+//         console.log('10 seconds later...');
+//     }, 5000);
+
+// }, 5000);
+
+// console.log('end');
+
+// function delay() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(resolve, 5000);
+//     })
+// }
+
+// delay()
+//     .then(() => {
+//         console.log('5 seconds later...');
+//     });
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+       console.log(data); 
+    })
